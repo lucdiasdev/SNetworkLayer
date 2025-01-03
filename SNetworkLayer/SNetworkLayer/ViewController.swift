c557036e8e5284d6decc7040e3e45832311bfa8a
lucdiasdev <lucdias@uolinc.com>
2025-01-03 13:09:32 -0300

    initial commit

---
+++ SNetworkLayer/SNetworkLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SNetworkLayer
+//
+//  Created by Lucas Rodrigues Dias on 03/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
